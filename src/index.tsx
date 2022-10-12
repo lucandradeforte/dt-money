2debe1a82979f84c9b41ec6653dcc4a81681c889
Lucas De Andrade Forte <lucasdeandradeforte@gmail.com>
2022-10-11 23:30:56 -0300

    :sparkles: feat: Configuração MirageJs e Axios - API mocking

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,30 @@
+import { createServer } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
-const root = ReactDOM.createRoot(
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          creatAt: new Date()
+        }
+      ]
+    })
+  },
+})
+
+ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
-root.render(
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
